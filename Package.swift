63be94ce27e7a8538fb46d7118312a6fdcbd3523
Magnus Nilsson <magnus.nilsson@newseed.se>
2025-02-25 11:02:37 +0100

    Exposing headers

--- Package.swift
+++ Package.swift
@@ -23,7 +23,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TSMobileAnalyticsObjC",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources/TSMobileAnalyticsObjC",
+            publicHeadersPath: "include"
+        ),
         .target(
             name: "TSMobileAnalytics",
             dependencies: [],
